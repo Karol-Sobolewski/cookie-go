c18254693101d263928dec7942400a8eb70c6b27
Karol <karolsobolewski92@gmail.com>
2020-12-12 21:36:38 +0100

    Add images to github repo and on SVR

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, `../public/images`)));
-
+app.use(express.static(path.join(__dirname, `../build`)));
 // app.use(express.static(path.join(__dirname, `../public`)));
 
 /* API ENDPOINTS */
