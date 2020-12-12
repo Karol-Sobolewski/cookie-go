20a2179ac3dbeca512a52597997ccc8b92f7afb0
Karol <karolsobolewski92@gmail.com>
2020-12-12 20:40:56 +0100

    Try to add remote DB with data directly in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,9 @@ const productsRoutes = require(`./routes/products.routes`);
 const app = express();
 
 /* MIDDLEWARE */
-app.use(cors());
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 // app.use(express.static(path.join(__dirname, `../public`)));
 app.use(express.static(path.join(__dirname, `../build`)));
@@ -22,14 +22,16 @@ app.use(`/api`, menusRoutes);
 app.use(`/api`, pagesRoutes);
 app.use(`/api`, productsRoutes);
 
-/* API ERROR PAGES */
-app.use(`/api`, (req, res) => {
-  res.status(404).send({ data: `Not found...` });
-});
+
 
 /* REACT WEBSITE */
 app.use(`*`, (req, res) => {
   res.sendFile(path.join(__dirname, `../build/index.html`));
+});
+
+/* API ERROR PAGES */
+app.use(`/api`, (req, res) => {
+  res.status(404).send({ data: `Not found...` });
 });
 
 /* MONGOOSE */
